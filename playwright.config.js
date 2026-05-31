5dd15dea23c8d70d60432e407fed44db3985d6b7
ytsuyuzaki <yudai.tsuyuzaki@gmail.com>
2026-05-31 08:11:54 +0000

    feat: add storage state and global setup enhancements for Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from '@playwright/test';
 
-const baseConfig = require( '@wordpress/scripts/config/playwright.config.js' );
+const baseConfig = require('@wordpress/scripts/config/playwright.config.js');
 
-const config = defineConfig( {
+const config = defineConfig({
 	...baseConfig,
-	globalSetup: require.resolve( './tests/e2e/global-setup.js' ),
+	globalSetup: require.resolve('./tests/e2e/global-setup.js'),
 	testDir: './tests/e2e',
-} );
+	webServer: {
+		...baseConfig.webServer,
+		command: 'npm run wp-env:start && npx wp-env run tests-cli wp core update-db',
+	},
+});
 
 export default config;
--- tests/e2e/global-setup.js
+++ tests/e2e/global-setup.js
@@ -5,29 +5,46 @@ import { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
  * @param {import('@playwright/test').FullConfig} config
  * @return {Promise<void>}
  */
-async function globalSetup( config ) {
-	const { storageState, baseURL } = config.projects[ 0 ].use;
+async function globalSetup(config) {
+	const { storageState, baseURL } = config.projects[0].use;
 	const storageStatePath =
 		typeof storageState === 'string' ? storageState : undefined;
 
-	const requestContext = await request.newContext( {
+	const requestContext = await request.newContext({
 		baseURL,
-	} );
+	});
 
-	const requestUtils = new RequestUtils( requestContext, {
+	const requestUtils = new RequestUtils(requestContext, {
 		storageStatePath,
-	} );
+	});
 
 	// Authenticate and save the storageState to disk.
 	await requestUtils.setupRest();
 
+	const themeCandidates = [
+		'twentytwentyone',
+		'twentytwentytwo',
+		'twentytwentythree',
+		'twentytwentyfour',
+		'twentytwentyfive',
+		'twentytwentysix',
+	];
+
+	for (const themeSlug of themeCandidates) {
+		try {
+			await requestUtils.activateTheme(themeSlug);
+			break;
+		} catch (error) {
+			// Ignore missing themes; CI images can differ by bundled default themes.
+		}
+	}
+
 	// Reset the test environment before running the tests.
-	await Promise.all( [
-		requestUtils.activateTheme( 'twentytwentyone' ),
+	await Promise.all([
 		requestUtils.deleteAllPosts(),
 		requestUtils.deleteAllBlocks(),
 		requestUtils.resetPreferences(),
-	] );
+	]);
 
 	await requestContext.dispose();
 }
